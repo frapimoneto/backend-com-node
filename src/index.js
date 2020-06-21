c3ce2f6d9f2f2995a3ad61ea13483dd95daab875
Francisco Pinto Moreira Neto <31639140+frapimoneto@users.noreply.github.com>
2020-06-20 23:26:19 -0300

    .

--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@ app.use(express.json());
 const projects = [];
 
 app.get('/projects', (request, response) => {
-  return response.json(projects);
+  const { title } = request.query;
+
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+
+  return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
@@ -22,21 +28,36 @@ app.post('/projects', (request, response) => {
 })
 
 app.put('/projects/:id', (request, response) => {
-  return response.json([
-    "Projeto 5",
-    "Projeto 2",
-    "Projeto 3",
-    "Projeto 4"
-  ]);
+  const { id } = request.params;
+  const { title, owner } = request.body;
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' })
+  };
+
+  const project = {
+    id,
+    title,
+    owner
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 })
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    "Projeto 1",
-    "Projeto 2",
-    "Projeto 3",
-    "Projeto 4"
-  ]);
+  const { id } = request.params;
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' })
+  };
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 })
 
 app.listen(3333, () => {
